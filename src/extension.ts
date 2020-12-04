aa8d51fb2a2404f2638c1b329daf5022f564f9ba
从无到有 <madapeng@acoinfo.com>
2020-12-04 16:32:32 +0800

    增加手机模拟器

--- src/extension.ts
+++ src/extension.ts
@@ -9,29 +9,80 @@
  * Author: Li Qiang <liqiang@acoinfo.com>
  *
  */
-import { init, localize }   from "./utils/locale";
+import { init } from './utils/locale';
 
 import * as vscode from 'vscode';
 import { Edger, EdgerDeivceProvider } from './edgerDeviceProvider';
 import { EdgerApi } from './edgerApi';
-
+import { showPhone } from './mobile/mobile.browser';
+import { MobileType } from './mobile/model';
+import { debug } from 'console';
 
 process.on('uncaughtException', function (err) {
-  console.error( err);
+  console.error(err);
 });
 
 export function activate(context: vscode.ExtensionContext) {
- 	init(context.extensionPath);
-	const edgerDeivceProvider = new EdgerDeivceProvider(context);
-	vscode.window.registerTreeDataProvider('edgerDeviceView', edgerDeivceProvider);
-	vscode.commands.registerCommand('edgerDevices.refreshDevice', () => edgerDeivceProvider.refresh());
-	vscode.commands.registerCommand('edgerDevices.addDevice', () => edgerDeivceProvider.addDevice());
-	vscode.commands.registerCommand('edgerDevices.updateDevice', (edger: Edger) => edgerDeivceProvider.updateDevice(edger));
-	vscode.commands.registerCommand('edgerDevices.deleteDevice', (edger: Edger) => edgerDeivceProvider.deleteDevice(edger));
-	vscode.commands.registerCommand('edgerDevices.openConsole', (edger: Edger) => edgerDeivceProvider.openConsole(edger));
-	const edgerApi = new EdgerApi(context);
-	vscode.commands.registerCommand('edgerDevices.installApp', (edger: Edger) => edgerApi.install(edger));
-	vscode.commands.registerCommand('edgerDevices.archive', () => edgerApi.archive());
+  init(context.extensionPath);
+  const edgerDeivceProvider = new EdgerDeivceProvider(context);
+  vscode.window.registerTreeDataProvider(
+    'edgerDeviceView',
+    edgerDeivceProvider
+  );
+  vscode.commands.registerCommand('edgerDevices.refreshDevice', () =>
+    edgerDeivceProvider.refresh()
+  );
+  vscode.commands.registerCommand('edgerDevices.addDevice', () =>
+    edgerDeivceProvider.addDevice()
+  );
+  vscode.commands.registerCommand('edgerDevices.updateDevice', (edger: Edger) =>
+    edgerDeivceProvider.updateDevice(edger)
+  );
+  vscode.commands.registerCommand('edgerDevices.deleteDevice', (edger: Edger) =>
+    edgerDeivceProvider.deleteDevice(edger)
+  );
+  vscode.commands.registerCommand('edgerDevices.openConsole', (edger: Edger) =>
+    edgerDeivceProvider.openConsole(edger)
+  );
+  const edgerApi = new EdgerApi(context);
+  vscode.commands.registerCommand('edgerDevices.installApp', (edger: Edger) =>
+    edgerApi.install(edger)
+  );
+  vscode.commands.registerCommand('edgerDevices.archive', () =>
+    edgerApi.archive()
+  );
+  //
+  const mobilePreview = vscode.commands.registerCommand(
+    'mobile.preview',
+    (deviceItem: IDeviceItem) => {
+      let url = `https://${deviceItem.deviceIP}`;
+      showPhone(context, MobileType.IPhoneX, url);
+    }
+  );
+  context.subscriptions.push(mobilePreview);
+  //
+  //
+  //
+  const changeMobile = vscode.commands.registerCommand(
+    'mobile.change',
+    (url: string, mobileType: string) => {
+      const _type: string = mobileType.toLowerCase();
+      showPhone(context, _type as MobileType, url);
+    }
+  );
+  context.subscriptions.push(changeMobile);
 }
 
-export function deactivate() { }
+interface IDeviceItem {
+  contextValue: string;
+  deviceIP: string;
+  deviceName: string;
+  devicePass: string;
+  label: string;
+  iconPath: {
+    dark: string;
+    light: string;
+  };
+}
+
+export function deactivate() {}
