cf008529ca5620693ea5a78431e3bbc32390b4c1
FuWenHao <fuwenhao@acoinfo.com>
2021-04-20 20:52:17 +0800

    optimize the setconfig,openlog function

--- src/registers/commands/showConfigView.ts
+++ src/registers/commands/showConfigView.ts
@@ -2,7 +2,7 @@
  * @Author: FuWenHao  
  * @Date: 2021-04-10 15:11:00 
  * @Last Modified by: FuWenHao 
- * @Last Modified time: 2021-04-20 19:09:17
+ * @Last Modified time: 2021-04-20 20:29:00
  */
 import * as vscode from 'vscode';
 import * as config from '../../lib/config';
@@ -17,21 +17,39 @@ export = function (context: vscode.ExtensionContext) {
   let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
   let disposable = vscode.commands.registerCommand('edgeros.setConfig', async (...options: any[]) => {
-    currentPanel = vscode.window.createWebviewPanel('setConfigView', 'config', vscode.ViewColumn.One, {
-      enableScripts: true
-    });
 
-    // set title icon
-    currentPanel.iconPath = vscode.Uri.parse(config.edgerosLogo);
-    // set html/js path
-    let webViewFileName = 'setConfig';
-    // get vue,element,css uri
-    let assetUris = await common.getWebViewBaseUris(webViewFileName, currentPanel, context);
-    // set html 
-    currentPanel.webview.html = await ejs.renderFile(path.join(context.extensionPath, 'view', webViewFileName, 'view.ejs'), {
-      ...assetUris,
-    });
+    const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+    if (currentPanel) {
+      currentPanel.reveal(columnToShowIn);
+    } else {
+      currentPanel = vscode.window.createWebviewPanel('setConfigView', 'config', vscode.ViewColumn.One, {
+        enableScripts: true
+      });
+      // set title icon
+      currentPanel.iconPath = vscode.Uri.parse(config.edgerosLogo);
+      // set html/js path
+      let webViewFileName = 'setConfig';
+      // get vue,element,css uri
+      let assetUris = await common.getWebViewBaseUris(webViewFileName, currentPanel, context);
+      // set html 
+      currentPanel.webview.html = await ejs.renderFile(path.join(context.extensionPath, 'view', webViewFileName, 'view.ejs'), {
+        ...assetUris,
+      });
 
+
+      //reception webview message
+      currentPanel.webview.onDidReceiveMessage(async message => {
+        console.log("message");
+      });
+      // webview close trigger
+      currentPanel.onDidDispose(
+        () => {
+          currentPanel = undefined;
+        },
+        null,
+        context.subscriptions
+      );
+    }
   });
   context.subscriptions.push(disposable);
 };
--- view/setConfig/index.js
+++ view/setConfig/index.js
@@ -1,74 +1,29 @@
 const vscode = acquireVsCodeApi();
 const previousState = vscode.getState();
-var devicesList = [];
+
 const app = new Vue({
   el: '#app',
   data: () => {
-
-    var checkDevIp = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error('ip不能为空'));
-      }
-      if (/^\d+\.\d+\.\d+\.\d+$/g.test(value)) {
-        let devItem = devicesList.find(item => {
-          return item.devIp === value;
-        });
-        if (devItem) {
-          callback(new Error('已存在相同IP设备'));
-        } else {
-          callback();
-        }
-      } else {
-        callback(new Error('请输入正确IP地址'));
-      }
-    };
-
-    var checkDevName = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error('设备名不能为空'));
-      }
-      let devItem = devicesList.find(item => {
-        return item.devName === value;
-      });
-      if (devItem) {
-        callback(new Error('存在相同名称设备'));
-      } else {
-        callback();
-      }
-    };
-
     return previousState?.data || {
-      viewType: 'add',
+      type: "1",
+      version: "1"
     };
   },
   filters: {},
   watch: {
+
   },
   methods: {
     inputChange() {
       vscode.setState({ data: this.$data });
     },
-    onAddDevice() {
-      this.$refs['form'].validate((valid) => {
-        if (valid) {
-          let cmdData = {
-            type: 'addDev',
-            data: {
-              ...this.form
-            }
-          };
-          vscode.postMessage(cmdData);
-        }
-      });
-    },
     onMessageFn(msg) {
-      if (!this.form.devIp && msg.type === '_getDeviceData') {
-        devicesList = msg.data.devices;
+      if (!this.form.devIp && msg.type === '_getConfig') {
       }
     }
   },
   created() {
-    vscode.postMessage({ type: 'getDeviceData' });
+    vscode.postMessage({ type: 'getConfig' });
   }
 });
 app.$on("onmessage", function (msg) {
